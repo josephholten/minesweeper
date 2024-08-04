1291d8c2a77962fe4b81559ecde18c0dc078fdd5
Joseph Holten <joseph@holten.com>
2024-08-04 11:54:40 +0200

    introduce nice matrix class

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -8,6 +8,21 @@
 
 #include "util.h"
 
+namespace minesweeper {
+template<typename T>
+struct Matrix {
+    size_t rows;
+    size_t cols;
+    std::vector<T> data;
+
+    Matrix(size_t _rows, size_t _cols, T init) : rows{_rows}, cols{_cols}, data(rows*cols, init) {};
+
+    T* operator[](size_t i) {
+        return data.data() + i * cols;
+    }
+};
+}
+
 void DrawNet(Vector2 screenSize, Vector2 margin, float thick, Color color) {
     Vector2 nw {margin.x, margin.y};
     Vector2 ne {screenSize.x-margin.x, margin.y};
@@ -28,14 +43,14 @@ void DrawNet(Vector2 screenSize, Vector2 margin, float thick, Color color) {
     DrawLineExDashed(Vector2Add(sw, verticalLineOffset), Vector2Add(nw, verticalLineOffset), thick, color, dashLength);
 }
 
-void mines_rand_p(std::vector<bool>& mines, double p) {
+void mines_rand_p(std::vector<uint8_t>& mines, double p) {
     std::default_random_engine random_engine(std::random_device{}());
     std::bernoulli_distribution bernoulli(p);
     for (size_t i = 0; i < mines.size(); i++)
         mines[i] = bernoulli(random_engine);
 }
 
-void mines_rand_m(std::vector<bool>& mines, size_t m) {
+void mines_rand_m(std::vector<uint8_t>& mines, size_t m) {
     std::vector<size_t> idxs(mines.size(), 0);
     for (size_t i = 0; i < mines.size(); i++)
         idxs[i] = i;
@@ -45,8 +60,15 @@ void mines_rand_m(std::vector<bool>& mines, size_t m) {
         std::random_device()
     );
     for (size_t i = 0; i < m; i++)
-        mines[idxs[i]] = true;
+        mines[idxs[i]] = 1;
 }
+
+enum class TileState {
+    Untouched,
+    Empty,
+    Bomb,
+    Flag,
+};
 
 int main(int, char**){
     /*
@@ -64,11 +86,17 @@ int main(int, char**){
     Vector2 boxSize = {32, 32};
     Vector2 boxMargin = {4, 4};
     Color boxColor = foregroundColor;
+    Color emptyColor = GRAY;
 
     float borderThickness = 8;
     Color borderColor = foregroundColor;
 
     Vector2 boxes = {20, 15};
+
+    Vector2 boxesOffset = {
+        borderThickness + boxMargin.x,
+        borderThickness + boxMargin.y
+    };
 
     Vector2 screenSize = {
         2*borderThickness + boxMargin.x + boxes.x * (boxSize.x+boxMargin.x),
@@ -100,29 +128,68 @@ int main(int, char**){
     double mineProb = 0.1;
     size_t mineCount = 15;
 
-    std::vector<bool> mines(boxes.x * boxes.y, false);
-    mines_rand_m(mines, mineCount);
+    minesweeper::Matrix<uint8_t> mines(boxes.y, boxes.x, false);
+    mines_rand_m(mines.data, mineCount);
+
+    minesweeper::Matrix<TileState> tileStates((size_t)boxes.y, (size_t)boxes.x, TileState::Untouched);
+    minesweeper::Matrix<size_t> bombCount((size_t)boxes.y, (size_t)boxes.x, 0);
 
     // close with ESC
     while(!WindowShouldClose()) {
+        // handle input
+        Vector2 mousePos = GetMousePosition();
+        if (mousePos.x >= boxesOffset.x && mousePos.x <= screenSize.x - boxesOffset.x
+            && mousePos.y >= boxesOffset.y && mousePos.y <= screenSize.y - boxesOffset.y)
+        {
+            size_t mouseBoxX = (mousePos.x - boxesOffset.x) / (boxSize.x + boxMargin.x);
+            size_t mouseBoxY = (mousePos.y - boxesOffset.y) / (boxSize.y + boxMargin.y);
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+                tileStates[mouseBoxY][mouseBoxX] = TileState::Flag;
+            }
+
+            else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                tileStates[mouseBoxY][mouseBoxX] = (mines[mouseBoxY][mouseBoxX] ? TileState::Bomb : TileState::Empty);
+            }
+        }
+
         BeginDrawing(); {
             ClearBackground(backgroundColor);
             // draw border
             Rectangle borderRec = {0, 0, screenSize.x, screenSize.y};
             DrawRectangleLinesEx(borderRec, borderThickness, borderColor);
 
-            Vector2 boxesOffset = {
-                borderThickness + boxMargin.x,
-                borderThickness + boxMargin.y
-            };
-
             for (size_t iy = 0; iy < boxes.y; iy++) {
                 for (size_t ix = 0; ix < boxes.x; ix++) {
                     float x = boxesOffset.x + ix*(boxSize.x + boxMargin.x);
                     float y = boxesOffset.y + iy*(boxSize.y + boxMargin.y);
-                    DrawRectangleV({x, y}, boxSize, boxColor);
-                    if (mines[iy*(size_t)boxes.x + ix])
+
+                    switch (tileStates[iy][ix]) {
+                    case TileState::Untouched:
+                        DrawRectangleV({x, y}, boxSize, boxColor);
+                        if (mines[iy][ix])
+                            DrawTextureV(bomb, {x, y}, BLACK);
+                        break;
+
+                    case TileState::Empty:
+                        DrawRectangleV({x, y}, boxSize, emptyColor);
+                        break;
+
+                    case TileState::Flag:
+                        DrawRectangleV({x, y}, boxSize, boxColor);
+                        DrawTextureV(flag, {x, y}, WHITE);
+                        break;
+
+                    case TileState::Bomb:
+                        DrawRectangleV({x, y}, boxSize, boxColor);
                         DrawTextureV(bomb, {x, y}, WHITE);
+                        break;
+
+                    default:
+                        TraceLog(LOG_FATAL, "unrecognized tile state!");
+                        abort();
+                        break;
+                    }
                 }
             }
 
